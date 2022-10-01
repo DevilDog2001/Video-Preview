122910141d74787bb80e2b8e690c620fbca39c9c
Richardson Antoine <91235784+DevilDog2001@users.noreply.github.com>
2022-09-30 21:15:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("page loaded...");
+var x = document.getElementById("myvideo");
+
+function plays(){
+    x.play();
+}
+function pauses(){
+    x.pause();
+    x.currentTime = 0;
+}
+
+var y = document.getElementById("btn1");
+
+function subs(){
+    y.innerText = "Unsubscribe";
+}
+
+var z = document.getElementById("btn");
+
+function Logout(){
+    z.innerText = "Logout";
+}
